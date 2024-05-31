5212fbf918cf84aa79e84aadff8772a8adfa1a6d
alxdfm <alexandre_fmachado@hotmail.com>
2024-05-31 17:15:58 -0300

    feat: add router

---
+++ src/presentation/routers/order-router.ts
@@ -0,0 +1,21 @@
+import express from 'express';
+import { Request, Response } from 'express';
+import { ErrorType } from '../types';
+import { INewOrderUseCase } from '../../domain/interfaces/use-cases/new-order';
+
+export default function OrderRouter(newOrderUseCase: INewOrderUseCase) {
+  const router = express.Router();
+
+  router.post('/orders', async (req: Request, res: Response) => {
+    try {
+      const order = await newOrderUseCase.execute(req.body);
+      res.send(order);
+    } catch (err) {
+      console.log(err);
+      const error: ErrorType = { error: 'could not place order' };
+      res.status(500).send(error);
+    }
+  });
+
+  return router;
+}
