0fc4f55bb319ba1e0a5c8609b8c0af4aa0f2d752
alxdfm <alexandre_fmachado@hotmail.com>
2024-06-02 21:53:58 -0300

    fix: remove checkData middleware

--- src/presentation/routers/order-router.ts
+++ src/presentation/routers/order-router.ts
@@ -1,27 +1,20 @@
-import express, { NextFunction } from 'express';
+import express from 'express';
 import { Request, Response } from 'express';
 import { ErrorType } from '../types';
 import { INewOrderUseCase } from '../../domain/interfaces/use-cases/new-order';
-import { checkOrderDataMiddleware } from '../middlewares/check-order-data-middleware';
 
 export default function OrderRouter(newOrderUseCase: INewOrderUseCase) {
   const router = express.Router();
 
-  router.post(
-    '/order',
-    async (req: Request, res: Response, next: NextFunction) => {
-      checkOrderDataMiddleware(req, res, next);
-    },
-    async (req: Request, res: Response) => {
-      try {
-        const order = await newOrderUseCase.execute(req.body);
-        return res.status(201).send(order);
-      } catch (err) {
-        const error: ErrorType = { error: 'could not place order' };
-        return res.status(500).send(error);
-      }
-    },
-  );
+  router.post('/order', async (req: Request, res: Response) => {
+    try {
+      const order = await newOrderUseCase.execute(req.body);
+      return res.status(201).send(order);
+    } catch (err) {
+      const error: ErrorType = { error: 'could not place order' };
+      return res.status(500).send(error);
+    }
+  });
 
   return router;
 }
