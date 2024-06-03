565d71dc973379f061fd6b00ae49100d473d15c3
alxdfm <alexandre_fmachado@hotmail.com>
2024-06-03 00:57:47 -0300

    debug

--- src/server.ts
+++ src/server.ts
@@ -2,7 +2,10 @@ import express from 'express';
 import cors from 'cors';
 
 const server = express();
-server.use(cors());
+server.use(async (req, res, next) => {
+  console.log(req.body);
+  cors();
+});
 server.use(express.json());
 
 export default server;
